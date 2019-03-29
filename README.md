f824705f656ba436534a5fe654546208438d3af0
clgerhardt <lieblegerhardt@gmail.com>
2019-03-29 10:54:04 -0400

    adding DM, and updating the readme

--- README.md
+++ README.md
@@ -2,10 +2,70 @@
 
 ## Client
 
+Global Variables:
+- String: hostname
+- int: port
+- String: username
+- Thread: OutputThread
+- Thread: InputThread
+
+Methods:
+- Client()
+- start_client()
+- set_uesrname(String username)
+- get_username()
+- main(String [] args)
+
+## Output Thread sub class of Client class
+
+Global Variables:
+- BufferedReader: reader
+- Socket: socket
+- Client: client
+
+Methods:
+- OutputThread(Socket socket, Client client)
+- run()
+
+## Input Thread sub class of Client class
+
+Global Variables:
+- PrintWriter: writer
+- Socket: socket
+- Client: client
+
+Methods:
+- InputThread(Socket socket, Client client)
+- run()
 
 ## Server
 
+Global Variables:
+- int: port
 - Set<String>: usernames
+- Set<UserThreads>: user_threads
+- HashMap<String, UserThread> user_map
 
-- Set<Threads>: UserThreads
+Methods:
+- Server()
+- run_server()
+- send_to_group(String message, UserThread dont_send_to_this_user)
+- sent_to_person(String message, String specific_user, UserThread user)
+- add_username(String username, UserThread user_thread)
+- remove_user(String user_name, UserThread user)
+- get_username()
+- main(String [] args)
+
+## User Thread sub class of Server class
+
+Global Variables:
+- Socket: socket
+- Server: server
+- PrintWriter: writer
+
+Methods:
+- UserThread(Socket socket, Server server)
+- send_message(String message)
+- run()
+
 
--- Server.java
+++ Server.java
@@ -6,6 +6,7 @@ public class Server{
         private int port;
         private Set<String> usernames = new HashSet<>();
         private Set<UserThread> user_threads = new HashSet<>();
+        private HashMap<String, UserThread> user_map = new HashMap<>();
      
         public Server() {
             this.port = 6013;
@@ -31,30 +32,34 @@ public class Server{
             }
         } 
 
-        void send_to_group(String message, UserThread exclude_user) {
+        void send_to_group(String message, UserThread dont_sent_to_this_user) {
             for (UserThread u : user_threads) {
-                if (u != exclude_user) {
+                if (u != dont_sent_to_this_user) {
                     u.sendMessage(message);
                 }
             }
         }
 
-        void send_to_person(String message, UserThread specific_user){
-            for (UserThread u : user_threads) {
-                if (u == specific_user) {
-                    u.sendMessage(message);
-                }
+        void send_to_person(String message, String specific_user, UserThread user){
+            if(user_map.containsKey(specific_user)){
+                UserThread u = user_map.get(specific_user);
+                u.sendMessage(message);
+            }
+            else{
+                user.sendMessage("This user not in chat. Make sure you spelled their name correctly.");
             }
         }
      
-        void add_username(String username) {
+        void add_username(String username, UserThread user_thread) {
             usernames.add(username);
+            user_map.put(username, user_thread);
         }
      
         void remove_user(String user_name, UserThread user) {
             boolean removed = usernames.remove(user_name);
             if (removed) {
                 user_threads.remove(user);
+                user_map.remove(user_name);
                 System.out.println("The user " + user_name + " left");
             }
         }
@@ -77,6 +82,10 @@ public class Server{
                 this.socket = socket;
                 this.server = server;
             }
+
+            void sendMessage(String message) {
+                writer.println(message);
+            }
          
             public void run() {
                 try {
@@ -87,7 +96,7 @@ public class Server{
                     writer = new PrintWriter(output, true);
                   
                     String username = reader.readLine();
-                    server.add_username(username);
+                    server.add_username(username, this);
          
                     String server_message = "New user connected: " + username;
                     server.send_to_group(server_message, this);
@@ -95,8 +104,16 @@ public class Server{
                     String clientMessage= "";
                     while (!clientMessage.equals("quit")){
                         clientMessage = reader.readLine();
-                        server_message = username + ": " + clientMessage;
-                        server.send_to_group(server_message, this);
+                        System.out.println(clientMessage.startsWith("@"));
+                        if(clientMessage.startsWith("@")){
+                            String [] result = clientMessage.split(" ", 2);
+                            server_message = username + ": " + result[1];
+                            server.send_to_person(server_message, result[0].substring(1), this);
+                        }
+                        else{
+                            server_message = username + ": " + clientMessage;
+                            server.send_to_group(server_message, this);
+                        }
                     }
          
                     server.remove_user(username, this);
@@ -110,9 +127,5 @@ public class Server{
                 }
             }
          
-            void sendMessage(String message) {
-                writer.println(message);
-            }
         }
-
 }
